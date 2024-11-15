c4aa2dfc6eaae3d80a03197ed7080d87c673583f
unknown <damirrahymzhanov20@gmail.ru>
2024-11-15 18:53:09 +0600

    my first commit

---
+++ Foodie/User/User.Master.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Foodie.User
+{
+    public partial class User : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Request.Url.AbsoluteUri.ToString().Contains("WebForm1.aspx"))
+                {
+                form1.Attributes.Add("class", "sub_page");
+                }
+            else
+            {
+                form1.Attributes.Remove("class");
+                Control sliderUserControl = (Control)Page.LoadControl("SliderController.ascx");
+
+
+                pnlSliderUC.Controls.Add(sliderUserControl);
+             }
+        }
+    }
+}
